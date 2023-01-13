4f8eb6ffd9bf72d0e69882a1cc3abcdf159a1e00
sharbel <sherbelhoma@hotmail.com>
2023-01-12 19:50:17 -0600

    initial files

--- src/server.cpp
+++ src/server.cpp
@@ -1,38 +1,6 @@
 #include <iostream>
-#include <cpprest/http_listener.h>
-
-using namespace std;
-using namespace web;
-using namespace web::http;
-using namespace web::http::experimental::listener;
 
 int main()
 {
-    // Specify the URI to listen to
-    http_listener listener(U("http://localhost:8080/"));
-
-    // Register a request handler
-    listener.support(methods::GET, [](http_request request) {
-        // Handle the request and return a response
-        http_response response(status_codes::OK);
-        response.set_body("Hello, World!");
-        request.reply(response);
-    });
-
-    // Start the listener
-    try {
-        listener
-            .open()
-            .then([&listener]() { cout << "Listening for requests at: " << listener.uri().to_string() << endl; })
-            .wait();
-    }
-    catch (exception const & e) {
-        cout << e.what() << endl;
-    }
-
-    // Wait for user input
-    cout << "Press Enter to exit." << endl;
-    cin.ignore();
-    listener.close();
     return 0;
 }
