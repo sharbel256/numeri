f708beb71ffe7942a9826d1a3fbf966d1dda864d
sharbel <sherbelhoma@hotmail.com>
2023-02-19 23:10:24 -0600

    add SSL

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,19 +1,21 @@
 #include "ui/mainwindow.h"
 #include <QApplication>
 #include <QString>
+#include <string>
 #include <iostream>
 #include "service.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
-{  
+{   
     Service service;
-    cout << service.test << endl;
+    service.sendMessage(R"({"type": "subscribe", "product_ids": ["ETH-USD"], "channels": ["level2"]})");
     QApplication a(argc, argv);
 
     QString message("Hello World!");
     MainWindow w;
     w.show();
+    
     return a.exec();
 }
--- src/service/service.h
+++ src/service/service.h
@@ -1,12 +1,66 @@
 #ifndef SERVICE_H
 #define SERVICE_H
 
+#include <boost/asio/ssl.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/websocket/ssl.hpp>
 #include <string>
+#include <iostream>
+
+using tcp = boost::asio::ip::tcp;
+namespace ssl = boost::asio::ssl;
+namespace websocket = boost::beast::websocket;
+namespace http = boost::beast::http;
 
 class Service {
 public:
-    std::string test;
-    Service();
+
+    
+
+    Service() {
+
+    };
+
+    void sendMessage(std::string message) {
+        try {
+            std::cout <<"beginning connection" << std::endl;
+            std::string host = "ws-feed.prime.coinbase.com";
+            std::string port = "443";
+            std::string path = "/";
+
+            boost::asio::io_context io_context;
+
+            // ssl::context ssl_context{ssl::context::sslv23_client};
+            ssl::context ssl_context{ssl::context::tlsv12_client};
+
+            tcp::resolver resolver(io_context);
+            ssl::stream<tcp::socket> ssl_stream(io_context, ssl_context);
+
+            auto const results = resolver.resolve(host, port);
+            boost::asio::connect(ssl_stream.lowest_layer(), results);
+            ssl_stream.handshake(ssl::stream_base::client);
+            std::cout <<"connection established" << std::endl;
+
+            websocket::stream<ssl::stream<tcp::socket>> websocket_stream(std::move(ssl_stream));
+            websocket_stream.set_option(websocket::stream_base::decorator([](websocket::request_type& req) {
+                req.set(http::field::host, "ws-feed.prime.coinbase.com");
+                req.set(http::field::upgrade, "websocket");
+                req.set(http::field::connection, "Upgrade");
+                req.set(http::field::sec_websocket_key, "x3JJHMbDL1EzLkh9GBhXDw==");
+                req.set(http::field::sec_websocket_version, "13");
+            }));
+            websocket_stream.handshake(host + ":" + port, path);
+            std::cout <<"websocket handshake complete" << std::endl;
+            websocket_stream.write(boost::asio::buffer(message));
+            boost::beast::multi_buffer buffer;
+            websocket_stream.read(buffer);
+            std::cout << boost::beast::make_printable(buffer.data()) << std::endl;
+        } catch (std::exception& e) {
+            std::cerr << "Exception: " << e.what() << std::endl;
+        }
+        
+    }
 };
 
 #endif
