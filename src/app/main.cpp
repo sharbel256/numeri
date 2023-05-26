d1c3db1c870dc8dfb75f1c0502b21664968ed932
sharbel <sherbelhoma@hotmail.com>
2023-05-25 21:46:32 -0500

    add websocket SSL async client

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,17 +3,44 @@
 #include <QString>
 #include <string>
 #include <iostream>
-#include "service.h"
+#include <thread>
+#include <client.cpp>
 
 using namespace std;
 
 int main(int argc, char *argv[])
-{   
+{
+    auto host = "ws-feed.exchange.coinbase.com";
+    auto port = "443";
+    auto text = R"({"type": "subscribe","channels": [{"name": "ticker", "product_ids": ["BTC-USD"]}]})";
+
+    // The io_context is required for all I/O
+    net::io_context ioc;
+
+    // The SSL context is required, and holds certificates
+    ssl::context ctx{ssl::context::tlsv13_client};
+
+    // This holds the root certificate used for verification
+    ctx.set_default_verify_paths();
+    
+
+    // Launch the asynchronous operation
+    std::make_shared<session>(ioc, ctx)->run(host, port, text);
+
+    // Run the I/O service. The call will return when
+    // the socket is closed.
+    // Let's run it in a separate thread.
+    std::thread io_thread = std::thread([&ioc](){ ioc.run(); });
+
     QApplication a(argc, argv);
 
-    QString message("Hello World!");
+    QString hello("Hello World!");
     MainWindow w;
     w.show();
     
-    return a.exec();
+    int ret = a.exec();
+    ioc.stop();
+    if (io_thread.joinable()) io_thread.join();
+
+    return ret;
 }
--- src/service/service.cpp
+++
@@ -1,4 +0,0 @@
-#include "service.h"
-#include <iostream>
-
-
