edaa28dd67e7ad70b38db50e67c48d09e46a2221
sharbel <sherbelhoma@hotmail.com>
2023-06-03 14:47:25 -0500

    cleanup

--- src/app/ui/mainwindow.cpp
+++ src/app/ui/mainwindow.cpp
@@ -1,12 +1,5 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include <thread>
-#include <QString>
-#include <string>
-#include <iostream>
-#include <QJsonDocument>
-#include <QJsonObject>
-
 
 MainWindow::MainWindow(WebSocketClient* client, QWidget *parent)
     : QMainWindow(parent)
@@ -40,11 +33,37 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
-void MainWindow::updateWithNewData(const QString& data)
+void MainWindow::runFunction()
 {
-    // Update your UI with the new data here
-    
+    // TODO: Remove hardcoded values
+    // TODO: Add error handling
+    // TODO: Add a stop button or handle multiple invocation of runFunction()
+    // TODO: Let the user decide host, port, and subscription
+    std::cout << "MainWindow::runFunction()" << std::endl;
+    auto host = "ws-feed.exchange.coinbase.com";
+    auto port = "443";
+    auto text = R"({"type": "subscribe","channels": [{"name": "ticker", "product_ids": ["BTC-USD"]}]})";
+
+    // The SSL context is required, and holds certificates
+    ssl::context ctx{ssl::context::tlsv13_client};
+    ctx.set_default_verify_paths();
+
+    websocket_client = std::make_shared<WebSocketClient>(ioc, ctx);
+
+    connect(websocket_client.get(), &WebSocketClient::newDataReceived, this, &MainWindow::processData);
+
+    try {
+        websocket_client->run(host, port, text);
+        io_thread = std::make_unique<std::thread>([this](){ ioc.run(); });
+    } catch (std::exception const& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }   
+}
+
+void MainWindow::processData(const QString& data)
+{
+    // TODO: create async data processing
+    std::cout << "MainWindow::processData()" << std::endl;
 
     // Convert the QString to QByteArray
     QByteArray jsonBytes = data.toUtf8();
@@ -58,30 +77,10 @@ void MainWindow::updateWithNewData(const QString& data)
     // Now you can access values from the JSON object, for example:
     QString price = json["price"].toString();
 
-    ui->btc_price->setText(price);
+    updatePrice(price);
 }
 
-void MainWindow::runFunction()
+void MainWindow::updatePrice(const QString& price)
 {
-    std::cout << "MainWindow::runFunction()" << std::endl;
-    auto host = "ws-feed.exchange.coinbase.com";
-    auto port = "443";
-    auto text = R"({"type": "subscribe","channels": [{"name": "ticker", "product_ids": ["BTC-USD"]}]})";
-
-    // The SSL context is required, and holds certificates
-    ssl::context ctx{ssl::context::tlsv13_client};
-
-    // This holds the root certificate used for verification
-    ctx.set_default_verify_paths();
-
-    websocket_client = std::make_shared<WebSocketClient>(ioc, ctx);
-
-    connect(websocket_client.get(), &WebSocketClient::newDataReceived, this, &MainWindow::updateWithNewData);
-
-    try {
-        websocket_client->run(host, port, text);
-        io_thread = std::make_unique<std::thread>([this](){ ioc.run(); });
-    } catch (std::exception const& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }   
+    ui->btc_price->setText(price);
 }
