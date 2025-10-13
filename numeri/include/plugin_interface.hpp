de9932521a259d4dc7f94da7d6becbcbc1a33e7f
sharbel <sherbelhoma@hotmail.com>
2025-10-12 20:52:13 -0500

    add test plugin

--- numeri/include/plugin_interface.hpp
+++ numeri/include/plugin_interface.hpp
@@ -5,7 +5,7 @@
 #include <string>
 
 class IPlugin {
- public:
+public:
   virtual ~IPlugin() = default;
   virtual std::string getName() const = 0;
   virtual void execute() = 0;
@@ -14,8 +14,12 @@ class IPlugin {
 using PluginCreateFunc = IPlugin* (*)();
 using PluginDestroyFunc = void (*)(IPlugin*);
 
-#define EXPORT_PLUGIN(PluginClass)                       \
-  extern "C" {                                           \
-  IPlugin* createPlugin() { return new PluginClass(); }  \
-  void destroyPlugin(IPlugin* plugin) { delete plugin; } \
+#define EXPORT_PLUGIN(PluginClass)                                                                   \
+  extern "C" {                                                                                       \
+  IPlugin* createPlugin() {                                                                          \
+    return new PluginClass();                                                                        \
+  }                                                                                                  \
+  void destroyPlugin(IPlugin* plugin) {                                                              \
+    delete plugin;                                                                                   \
+  }                                                                                                  \
   }
--- numeri/plugins/plugin1.cpp
+++ numeri/plugins/plugin1.cpp
@@ -1,11 +1,16 @@
-#include <iostream>
 #include <plugin_interface.hpp>
 
-class Plugin1 : public IPlugin {
- public:
-  std::string getName() const override { return "Plugin1"; }
+#include <iostream>
 
-  void execute() override { std::cout << "Hello from Plugin1!" << std::endl; }
+class Plugin1 : public IPlugin {
+public:
+  std::string getName() const override {
+    return "Plugin1";
+  }
+
+  void execute() override {
+    std::cout << "Hello from Plugin1!" << std::endl;
+  }
 };
 
 EXPORT_PLUGIN(Plugin1)
